a309db4ccf702707428949b9e052c16ce1e66144
fengliang <liang.feng@gnetoffice.com>
2017-03-17 18:30:40 +0800

    ss

--- first.py
+++ first.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-print('Hello')
+# -*- coding: utf-8 -*-
+print('Hello 你好')
+
+s1 = 72
+s2 = 85
+
+r = (85 - 72) / 72 * 100
+print('%.1f%%' % r)
+
+r = (s2-s1)/s1*100 
+print('成绩提升了：%.1f%%' % r)
