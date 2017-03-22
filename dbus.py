d91f74f05f3d560ebc9be19ca74b62066fca59d0
fwar34 <fwar34@126.com>
2017-03-22 15:24:17 +0800

    add dbus.py

---
+++ dbus.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+ 
+# File Name: dbus.py
+# Created Time: Wed 22 Mar 2017 03:14:50 PM CST
+# Author: Feng
+# Content: python调用dbus
+
+import dbus
+bus = dbus.SystemBus()
+bus_obj = bus.get_object('org.freedesktop.DBus', '/')
+iface = dbus.Interface(bus_obj, 'org.desktop.DBus')
+names = iface.ListActivatableNames()
+for n in names:
+    print n
