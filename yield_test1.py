ce7013c4c619eeddd559fe20d4e677b66cd2aff1
fwar34 <fwar34@126.com>
2017-03-23 20:25:09 +0800

    添加注释

--- yield_test1.py
+++ yield_test1.py
@@ -4,11 +4,11 @@
 # File Name: yield_test1.py
 # Author: Feng
 # Created Time: Thu 23 Mar 2017 08:13:14 PM CST
-# Content: 
+# Content: yield函数测试
 
 import sys
 
-def fibonacci(n):
+def fibonacci(n): #生成器函数--斐波那契数列
     a, b, counter = 0, 1, 0
     while True:
         if (counter > n):
@@ -17,7 +17,7 @@ def fibonacci(n):
         a, b = b, a + b
         counter += 1
 
-f = fibonacci(10)
+f = fibonacci(10)  #f是一个迭代器，由生成器函数返回
 
 while True:
     try:
