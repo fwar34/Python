3e7a9d6cbc32bb650747a853e75b5192faeb8948
fwar34 <fwar34@126.com>
2017-03-24 10:39:58 +0800

    添加嵌套列表

--- list_tuidao_test1.py
+++ list_tuidao_test1.py
@@ -19,3 +19,36 @@ print(v4)
 
 v5 = [x * 3 for x in v2 if x > 2]
 print(v5)
+
+#列表推导式可以使用复杂的表达式或者嵌套函数
+v6 = [str(round(355/113, i)) for i in range(1, 6)]
+print(v6)
+
+
+#python的列表还可以嵌套
+#3 * 4矩阵
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+]
+
+#以下将3X4的矩阵列表转换为4X3列表
+v7 = [[row[i] for row in matrix] for i in range(4)]
+print(v7)
+
+#也可以使用以下方法来实现
+v8 = []
+for i in range(4):
+    v8.append([row[i] for row in matrix])
+
+print(v8)
+
+#另外一种实现方法
+v9 = []
+for i in range(4):
+    vtemp = []
+    for row in matrix:
+        vtemp.append(row[i])
+    v9.append(vtemp)
+print(v9)
