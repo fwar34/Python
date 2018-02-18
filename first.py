bc051c2fb44ce4d05f229a8450d824c64c340022
fwar34 <fwar34@126.com>
2018-02-18 21:24:55 +0800

    a

--- first.py
+++ first.py
@@ -20,3 +20,35 @@ while n <= 99:
 print(L)
 
 print(L[0:3])
+
+str = 'abcdef'
+print(str[:3])
+print(str[::2])
+print(str[2:-2])
+for ele in str:
+    print(ele)
+
+from collections import Iterable
+print(isinstance('abc', Iterable))
+
+L = []
+for x in range(1, 11):
+    L.append(x * x)
+print(L)
+
+L = [x * x for x in range(1, 11)]
+print(L)
+
+L = [x * x for x in range(1, 11) if x % 2 == 0]
+print(L)
+
+L = [m + n for m in 'abc' for n in 'xyz']
+print(L)
+
+import os
+L = [d for d in os.listdir('.')]
+print(L)
+
+d = {'x' : 'a', 'y' : 'b', 'z' : 'c'}
+for k, v in d.items():
+    print(k, '=', v)
