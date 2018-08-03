06201bf60f381aba44843387b1b44b684206b6f6
fwar34 <fwar34@126.com>
2018-08-03 13:23:41 +0800

    add gpio test

--- Raspberry/test_gpio.py
+++ Raspberry/test_gpio.py
@@ -6,6 +6,16 @@
 # Created Time: Thu Aug  2 22:41:30 2018
 # Content: 
 
+import time
 import RPi.GPIO as GPIO
 
-GPIO.setmode(
+GPIO.setmode(GPIO.BOARD)
+GPIO_PIN = 40
+
+GPIO.setup(GPIO_PIN, GPIO.OUT)
+
+while True:
+    GPIO.output(GPIO_PIN, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(GPIO_PIN, GPIO.LOW)
+    time.sleep(0.5)
