4c17e27254ca2ee4b9679d9c94dc32688c5b640c
fwar34 <fwar34@126.com>
2018-08-07 19:34:40 +0800

    add raget2

--- Raspberry/get_net.py
+++ Raspberry/get_net.py
@@ -22,7 +22,8 @@ class RaspberryMonitorNetSpeed:
             'Cookie': 'n56u_cookie_bw_rt_tab=WAN',
             'Host': '192.168.123.1',
             'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+                    (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
             }
 
     #最近一次请求 
@@ -35,22 +36,23 @@ class RaspberryMonitorNetSpeed:
     def __init__(self, username, passwd):
         self.headers['Authorization'] = 'Basic ' + b64encode((username + ':' + passwd).encode()).decode()
         data = self.__get_wan_rx_and_tx()
-        # self.last_rbytes = data[0]
-        # self.last_tbytes = data[1]
-        # self.last_time = data[2]
+        if data:
+            self.last_rbytes = data[0]
+            self.last_tbytes = data[1]
+            self.last_time = data[2]
         print("data = ", data)
 
     def __get_wan_rx_and_tx(self):
         text = requests.get(self.url, headers = self.headers).text
-        print("text = ", text)
-        # try:
-            # rx = int(text.split(',')[25].lstrip('rx:').strip(), 16)
-            # tx = int(text.split(',')[26].lstrip('tx:').rstrip('}\n').strip(), 16)
-            # new_time = time()
-        # except (IndexError, ValueError, TypeError):
-            # return False
-        # return [rx, tx, new_time]
+        print(text)
+        try:
+            rx = int(text.split(',')[25].lstrip('rx:').strip(), 16)
+            tx = int(text.split(',')[26].lstrip('tx:').rstrip('}\n').strip(), 16)
+            new_time = time()
+        except (IndexError, ValueError, TypeError):
+            return False
+        return [rx, tx, new_time]
 
 if __name__ == '__main__':
-    RaspberryMonitorNetSpeed('admin', 'admin')
+    RaspberryMonitorNetSpeed('admin', '413162555')
 
---
+++ Raspberry/raget2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+ 
+# File Name: raget2.py
+# Author: Feng
+# Created Time: Tue Aug  7 16:47:52 2018
+# Content: 
+
+import os
+
+def getTemp():
+    return os.popen('vcgencmd measure_temp').read()[5:9]
+
+def getRam():
+    return os.popen('free | tail -n +2').readline()
+
+def getDisk():
+    return os.popen('df -h / | tail -n +2').readline()
+
+def getCpuUse():
+    return os.popen('top -n1 | awk \'/Cpus/{print $0}\'').readline()
+
+cpu_temp = getTemp()
+
+
