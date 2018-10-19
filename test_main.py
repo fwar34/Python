758aa5512f2f24dc30ea7e9afbd0ef2358a01b08
fwar34 <fwar34@126.com>
2018-10-19 19:18:48 +0800

    add test __main__

---
+++ test_main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+ 
+# File Name: test_main.py
+# Author: Feng
+# Created Time: Fri 19 Oct 2018 07:15:17 PM CST
+# Content: 
+
+def Test():
+    print("We are in ", __name__)
+
+if __name__ == '__main__':
+    Test()
---
+++ test_main_.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+ 
+# File Name: test_main_.py
+# Author: Feng
+# Created Time: Fri 19 Oct 2018 07:17:29 PM CST
+# Content: 
+from test_main import Test
+
+Test()
