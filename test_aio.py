c196f57e2ded90b5bf76c2b0058189cfae42cb62
fwar34 <fwar34@126.com>
2019-03-01 20:04:54 +0800

    add test_aio.py

---
+++ test_aio.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+ 
+# File Name: test_aio.py
+# Author: Feng
+# Created Time: 2019-03-01 17:42
+# Content: https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001432090171191d05dae6e129940518d1d6cf6eeaaa969000
+
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] Consuming %s...' % n)
+        r = '200 OK'
+
+def produce(c):
+    # c.send(None)等价于next(c), 前面的send不规范用法
+    next(c)
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return: %s' % r)
+    c.close()
+
+c = consumer()
+produce(c)
