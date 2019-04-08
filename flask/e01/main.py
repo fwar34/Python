d75522cb6e271ff4a5fba527a5ed04721b8af003
fwar34 <fwar34@126.com>
2019-04-08 19:35:18 +0800

    database

--- flask/e01/main.py
+++ flask/e01/main.py
@@ -9,14 +9,16 @@ from flask import Flask
 from flask import redirect
 from flask import render_template
 from flask import request
+import data_base
 
 app = Flask(__name__)
 
 from wtforms import Form, TextField, PasswordField, validators
 
 class LoginForm(Form):
-    username = TextField("username", [validators.Required()])
-    password = PasswordField("password", [validators.Required()])
+    username = TextField("username", [validators.DataRequired()])
+    age = TextField("age", [validators.DataRequired()])
+    password = PasswordField("password", [validators.DataRequired()])
 
 @app.route('/user', methods = ['GET', 'POST'])
 def login():
@@ -30,6 +32,14 @@ def login():
         else:
             message = "Login failed"
             return render_template('index.html', message = message, form = my_form)
+    return render_template('index.html', form = my_form)
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    my_form = LoginForm(request.form)
+    if request.method == 'POST':
+        data_base.add_user(my_form.username.data, my_form.age.data, my_form.password.data)
+        return "Register Successfully"
     return render_template('index.html', form = my_form)
 
 if __name__ == "__main__":
