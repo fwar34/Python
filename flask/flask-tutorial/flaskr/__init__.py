1a929d93e1773ba3701256689b5ba0006b091908
fwar34 <fwar34@126.com>
2019-04-12 21:34:23 +0800

    add flask tutorial

---
+++ flask/flask-tutorial/flaskr/__init__.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+ 
+# File Name: __init__.py
+# Author: Feng
+# Created Time: 2019-04-11 12:16
+# Content:
+
+import os
+from flask import Flask
+
+def create_app(test_config = None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config = True)
+    app.config.from_mapping(SECRET_KEY = 'dev', DATABASE = os.path.join(app.instance_path, 'flask.sqlite'))
+
+    if test_config == None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent = True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simply page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    # from . import db
+    from flaskr import db
+    db.init_app(app)
+
+    return app
