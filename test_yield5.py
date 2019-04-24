bb2a6ab5c88857a690684f67549ef0310768cc7b
fwar34 <fwar34@126.com>
2019-04-24 22:10:30 +0800

    modify

--- test_yield5.py
+++ test_yield5.py
@@ -1,11 +1,10 @@
+# https://blog.csdn.net/soonfly/article/details/78361819
 def fab(max):
     n, a, b = 0, 0, 1
     while n < max:
         yield b
         a, b = b, a + b
         n = n + 1
-
-f = fab(5)
 
 # 假设要在fab()的基础上实现一个函数，调用起始都要记录日志
 def wrapper(fun_iterable):
@@ -19,7 +18,6 @@ for i in wrap:
     print(i, end = ' ')
 
 # 现在使用yield from代替for循环
-import logging
 def wrapper2(fun_iterable):
     print('start')
     yield from fun_iterable #注意此处必须是一个可生成对象
