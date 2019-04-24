517562556366b62fbdf1ad73a06efcb72eebcd60
fwar34 <fwar34@126.com>
2019-04-24 22:06:14 +0800

    add test yield

--- test_yield3.py
+++ test_yield3.py
@@ -16,19 +16,3 @@ print(g.send(None)) # 这行启动了生成器(并且启动的时候只能传递
 print(g.send('Hello'))
 print(g.send(11111))
 print(g.send('e'))
-
-# yield from
-def g1():
-    yield range(5)  #yield就是将range这个可迭代对象直接返回了。
-
-def g2():
-    yield from range(5) #而yield from解析了range对象，将其中每一个item返回了。yield from iterable本质上等于for item in iterable: yield item的缩写版 
-
-it1 = g1()
-it2 = g2()
-
-for x in it1:
-    print(x)
-
-for x in it2:
-    print(x)
---
+++ test_yield5.py
@@ -0,0 +1,30 @@
+def fab(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+
+f = fab(5)
+
+# 假设要在fab()的基础上实现一个函数，调用起始都要记录日志
+def wrapper(fun_iterable):
+    print('start')
+    for item in fun_iterable:
+        yield item
+    print('end')
+
+wrap = wrapper(fab(5))
+for i in wrap:
+    print(i, end = ' ')
+
+# 现在使用yield from代替for循环
+import logging
+def wrapper2(fun_iterable):
+    print('start')
+    yield from fun_iterable #注意此处必须是一个可生成对象
+    print('end')
+
+wrap2 = wrapper2(fab(5))
+for i in wrap2:
+    print(i, end = ' ')
